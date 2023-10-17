5d11898275d29f0ae517e1339451e64329d38ba7
Falgor.C <tranhoa.7604@gmail.com>
2023-10-17 13:19:57 +0700

    must use laravel/breeze due to missing config on inertiajs.com

--- src/resources/js/Pages/Inertia.vue
+++ src/resources/js/Pages/Inertia.vue
@@ -1,4 +1,13 @@
-export default {
-  template: `
-    <h1>test</h1>`
-}
+<script setup>
+import { Head } from '@inertiajs/vue3'
+
+defineProps({})
+</script>
+
+<template>
+  <main>
+    <Head title="Welcome" />
+    <h1>Welcome</h1>
+    <p>Hello , welcome to your first Inertia app!</p>
+  </main>
+</template>
